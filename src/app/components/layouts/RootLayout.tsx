24c8a1301ed32efb24ce955e7d73f73a9143a8ac
Deva090909 <amitsdevrukhkar@gmail.com>
2026-06-01 12:56:52 +0530

    Add files via upload

--- src/app/components/layouts/RootLayout.tsx
+++ src/app/components/layouts/RootLayout.tsx
@@ -774,7 +774,9 @@ export function RootLayout() {
         <main className="flex-1 overflow-y-auto p-3 sm:p-4 md:p-6 lg:p-8 min-w-0">
           <RouteGuard />
           {/* key forces remount on path change OR role change */}
-          <Outlet key={`${location.pathname}__${currentRole}`} />
+          <ErrorBoundary key={`eb__${location.pathname}__${currentRole}`}>
+            <Outlet key={`${location.pathname}__${currentRole}`} />
+          </ErrorBoundary>
         </main>
       </div>
 
